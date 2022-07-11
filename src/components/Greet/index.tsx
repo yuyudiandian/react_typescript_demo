5eb8fab6ecbe69a003f1bf384afb251d3af04229
yuyuyudian <773514248@qq.com>
2022-07-12 00:20:12 +0800

    initial repository

---
+++ src/components/Greet/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type greet = {
+    name:string
+}
+
+const Greet = (props:greet) => {
+  return (
+    <div>
+          <h1>{props.name}</h1>
+    </div>
+  )
+}
+
+export default Greet
