f320425e564957fcdae884b4b7bbf2e416c36b1e
zhouyu65 <zhouyu65@meituan.com>
2022-07-12 20:58:48 +0800

    add component-props

---
+++ 11-component-props/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Private } from './components/auth/Private';
+import { Profile } from './components/auth/Profile';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Private isLoggedIn = {true} component = {Profile}/>
+        </div>
+    );
+}
+
+export default App;
